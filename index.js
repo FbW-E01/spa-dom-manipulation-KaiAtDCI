2ce3d2ffce318f43bce47e391511b80ee09a5ea6
KaiAtDCI <kaiatdci@web.de>
2021-06-16 16:56:56 +0200

    renaming, style

--- index.js
+++ index.js
@@ -1,15 +1,17 @@
 function createCard(book) {
     const imageUrl = book.img;
     const title = book.title;
-    const authorLastName = book.authorLastName;
-    const authorForeName = book.authorForeName;
-    const isRead = book.alreadyRead;
+    const lastName = book.authorLastName;
+    const foreName = book.authorForeName;
+    const alreadyRead = book.alreadyRead;
     return `
         <li class="book">
             <img class="book-cover" src="${imageUrl}" alt="cover">
             <div class="title">Title: ${title}</div>
-            <div class="author">Author: ${authorLastName}, ${authorForeName}</div>
-            <div class="status" style="background-color: ${isRead ? 'red' : 'green'}">Status: ${isRead ? 'read' : 'to read'}</div>
+            <div class="author">Author: ${lastName}, ${foreName}</div>
+            <div class="status" 
+                 style="background-color: ${alreadyRead ? 'red' : 'green'}">Status: ${alreadyRead ? 'read' : 'to read'}
+            </div>
         </li>`
 }
 
@@ -19,12 +21,12 @@ newBooksList = books.map(book => {
     return {
         img: book.img,
         title: book.title,
-        authorForeName: foreName,
-        authorLastName: lastName,
+        forename: foreName,
+        lastname: lastName,
         alreadyRead: book.alreadyRead,
     }
 })
 
-newBooksList.sort((a, b) => a.authorLastName >= b.authorLastName ? 1 : -1);
+newBooksList.sort((a, b) => a.lastname >= b.lastname ? 1 : -1);
 const bookList = document.getElementsByClassName('book-list').item(0);
 newBooksList.forEach(book => bookList.insertAdjacentHTML('beforeend', createCard(book)));
