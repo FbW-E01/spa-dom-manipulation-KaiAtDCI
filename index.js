cf244be0367f2f4656926ba58df07096587a1d50
KaiAtDCI <kaiatdci@web.de>
2021-06-16 16:01:16 +0200

    first commit

--- index.js
+++ index.js
@@ -1,16 +1,30 @@
 function createCard(book) {
     const imageUrl = book.img;
     const title = book.title;
-    const author = book.author;
+    const authorLastName = book.authorLastName;
+    const authorForeName = book.authorForeName;
     const isRead = book.alreadyRead;
     return `
         <li class="book">
             <img class="book-cover" src="${imageUrl}" alt="cover">
             <div class="title">Title: ${title}</div>
-            <div class="author">Author: ${author}</div>
+            <div class="author">Author: ${authorLastName}, ${authorForeName}</div>
             <div class="status" style="background-color: ${isRead ? 'red' : 'green'}">Status: ${isRead ? 'read' : 'to read'}</div>
         </li>`
 }
 
+newBooksList = books.map(book => {
+    const foreName = book.author.split(' ')[0];
+    const lastName = book.author.split(' ')[1];
+    return {
+        img: book.img,
+        title: book.title,
+        authorForeName: foreName,
+        authorLastName: lastName,
+        alreadyRead: book.alreadyRead,
+    }
+})
+
+newBooksList.sort((a, b) => a.authorLastName >= b.authorLastName ? 1 : -1);
 const bookList = document.getElementsByClassName('book-list').item(0);
-books.forEach(book => bookList.insertAdjacentHTML('beforeend', createCard(book)));
+newBooksList.forEach(book => bookList.insertAdjacentHTML('beforeend', createCard(book)));
